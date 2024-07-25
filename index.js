5544ba3da549f5885cd9423cb4f8da4caaae690d
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-07-25 15:27:46 +0300

    puhelinluettelon backend step2

--- index.js
+++ index.js
@@ -28,13 +28,16 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
 
-  app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response) => {
     response.json(persons)
-  })
+})
 
-  const PORT = 3001
-  app.listen(PORT, () => {
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`)
+})
+
+const PORT = 3001
+    app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
-
-console.log('test')
+})
