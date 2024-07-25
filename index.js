e92faff93908e805939e678883e566abce0bb633
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-07-25 15:44:18 +0300

    puhelinluettelon backend step4

--- index.js
+++ index.js
@@ -43,6 +43,12 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
 app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>`)
