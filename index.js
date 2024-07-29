e085c045d249d0ff2a29155a5c9102d357c684eb
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-07-29 14:48:25 +0300

    Puhelinluettelon backend step 9-10

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-var morgan = require('morgan')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 morgan.token('post-data', function (req) {
     return JSON.stringify(req.body)
@@ -90,7 +92,7 @@ app.get('/info', (request, response) => {
     <p>${new Date()}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
     app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
