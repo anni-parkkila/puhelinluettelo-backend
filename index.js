2d80754bf558a10dcb3f7120f9127fda24345487
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-08-07 14:43:54 +0300

    Step7

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', async (request, response) => {
+app.post('/api/persons', async (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
@@ -58,9 +58,11 @@ app.post('/api/persons', async (request, response) => {
         phonenumber: body.phonenumber
     })
 
-    person.save().then(savedPerson => {
+    person.save()
+    .then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -106,6 +108,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
--- models/person.js
+++ models/person.js
@@ -14,7 +14,11 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
   phonenumber: String,
 })
 
