2ceb2fbac66598d63e4e64fe17a2ab8ebda51b2d
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-08-07 18:11:15 +0300

    Validoinnin korjaus

--- models/person.js
+++ models/person.js
@@ -25,7 +25,7 @@ const personSchema = new mongoose.Schema({
     required: [true, 'Phone number required'],
     validate: {
       validator: function (v) {
-        return /\d{2,3}-\d{4,}$/.test(v);
+        return /^\d{2,3}-\d{4,}$/.test(v);
       },
       message: props => `${props.value} is not a valid phone number!`
     }
