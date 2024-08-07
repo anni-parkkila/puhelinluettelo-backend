0b9dafe5c46a400d7dc08cecad527ac954146166
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-08-07 20:06:32 +0300

    ESLint

--- models/person.js
+++ models/person.js
@@ -6,7 +6,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -25,7 +25,7 @@ const personSchema = new mongoose.Schema({
     required: [true, 'Phone number required'],
     validate: {
       validator: function (v) {
-        return /^\d{2,3}-\d{4,}$/.test(v);
+        return /^\d{2,3}-\d{4,}$/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
     }
--- mongo.js
+++ mongo.js
@@ -4,12 +4,12 @@ if (process.argv.length < 3) {
   console.log('Required to give password as argument')
   process.exit(1)
 } else if (process.argv.length === 4) {
-    console.log('Required to give password, person name and phonenumber as arguments to add a person to db')
-    process.exit(1)
+  console.log('Required to give password, person name and phonenumber as arguments to add a person to db')
+  process.exit(1)
 } else if (process.argv.length > 5) {
-    console.log('Too many parameters! Only password, person name and phonenumber allowed.')
-    console.log('Use quotation marks to add a two-part name or a number with spaces.')
-    process.exit(1)
+  console.log('Too many parameters! Only password, person name and phonenumber allowed.')
+  console.log('Use quotation marks to add a two-part name or a number with spaces.')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -28,24 +28,24 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        console.log('Phonebook:')
-        result.forEach(person => {
-          console.log(person.name, person.phonenumber)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    console.log('Phonebook:')
+    result.forEach(person => {
+      console.log(person.name, person.phonenumber)
     })
-} else if (process.argv.length === 5) {
-    const person = new Person({
-        name: process.argv[3],
-        phonenumber: process.argv[4]
-      })
-
-      person.save().then(result => {
-        console.log('Added', person.name, 'number', person.phonenumber, 'to phonebook')
-        mongoose.connection.close()
-      })
-} else {
-    console.log('Wrong number of parameters, closing connection')
     mongoose.connection.close()
+  })
+} else if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    phonenumber: process.argv[4]
+  })
+
+  person.save().then(() => {
+    console.log('Added', person.name, 'number', person.phonenumber, 'to phonebook')
+    mongoose.connection.close()
+  })
+} else {
+  console.log('Wrong number of parameters, closing connection')
+  mongoose.connection.close()
 }
