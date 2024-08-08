9d6de794961795857849fcafb3217874499e8269
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-08-08 14:39:47 +0300

    Lisää validointi päivitykseen

--- index.js
+++ index.js
@@ -66,14 +66,9 @@ app.post('/api/persons', async (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, phonenumber } = request.body
 
-  const person = {
-    name: body.name,
-    phonenumber: body.phonenumber
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, { name, phonenumber }, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
